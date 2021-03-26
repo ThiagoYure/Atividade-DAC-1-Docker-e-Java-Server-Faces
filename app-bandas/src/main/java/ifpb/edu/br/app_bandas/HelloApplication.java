1b641292ed642ebf77911f515f7303d08031f9af
ThiagoYure <thigoyure@gmail.com>
2021-03-26 08:36:08 -0300

    Criando o Hello World da atividade prática e dois Dockerfiles da parte 2 da atividade

---
+++ app-bandas/src/main/java/ifpb/edu/br/app_bandas/HelloApplication.java
@@ -0,0 +1,9 @@
+package ifpb.edu.br.app_bandas;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
